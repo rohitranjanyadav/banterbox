76a432225577601b481bbfd29badb5c5d5c93db5
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-01-25 20:15:37 +0545

    Add message model schema

---
+++ backend/models/message.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+
+    mediaType: {
+      type: String,
+      enum: ["image", "video"],
+      default: null,
+    },
+    mediaUrl: {
+      type: String,
+    },
+    mediaUrlPublicId: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
