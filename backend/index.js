41548bbe04a462958f4309d7dc96ede737a26b6d
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-01-25 19:38:08 +0545

    Initial setup for BanterBox MERN chat app

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
