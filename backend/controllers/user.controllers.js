04a0ee8cbf5ad02fcc71643dd8d6ec267d83e200
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-01-25 20:58:36 +0545

    Add user authentication controller

---
+++ backend/controllers/user.controllers.js
@@ -0,0 +1,120 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, user) => {
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "5y",
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 5 * 365 * 24 * 60 * 60 * 1000,
+  });
+};
+
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username) {
+      return res.status(422).json({ message: "Username is required!!!" });
+    }
+    if (!email) {
+      return res.status(422).json({ message: "Email is required!!!" });
+    }
+    if (!password) {
+      return res.status(422).json({ message: "Password is required!!!" });
+    }
+
+    const existUser = await User.findOne({ email });
+
+    if (existUser) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists!!!" });
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, 8);
+
+    const userData = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await userData.save();
+
+    generateToken(res, user);
+
+    const { password: pass, ...rest } = user._doc;
+
+    res.json({
+      success: true,
+      message: "User registered successfully!",
+      user: rest,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: "Error while registering!!!",
+      error,
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(422).json({ message: "Email is required!!!" });
+    }
+    if (!password) {
+      return res.status(422).json({ message: "Password is required!!!" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User doesn't exist!!!" });
+    }
+
+    const isMatchPassword = bcrypt.compareSync(password, user.password);
+
+    if (!isMatchPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    generateToken(res, user);
+
+    const { password: pass, ...rest } = user._doc;
+
+    res.json({
+      success: true,
+      message: "User login successful!",
+      user: rest,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: "Error while login!!!",
+      error,
+    });
+  }
+};
+
+export const logoutUser = async (req, res) => {
+  res.clearCookie("token");
+
+  res.json({ message: "Logged out successfully" });
+};
+
+
